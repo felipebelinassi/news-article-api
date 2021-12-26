e86a059a2282974e6d4f02d7c08a319219daed26
Felipe Belinassi <felipebelinassi@gmail.com>
2021-12-26 18:50:18 +0100

    2.1: add openAPI spec and swagger-ui lib

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,9 +1,22 @@
 import { Server } from 'http';
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import * as OpenApiValidator from 'express-openapi-validator';
+import { OpenAPIV3 } from 'express-openapi-validator/dist/framework/types';
+import apiSpec from '../../docs/apiSpec.json';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(apiSpec));
+app.use(
+  OpenApiValidator.middleware({
+    apiSpec: apiSpec as OpenAPIV3.Document,
+    validateRequests: true,
+    validateResponses: true,
+  }),
+);
 
 export const start = (port: string | number): Server =>
   app.listen(port, async () => {
