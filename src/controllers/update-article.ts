ff2c992bdeaf3290f57c31a1893e94375f78090e
Felipe Belinassi <felipebelinassi@gmail.com>
2021-12-27 19:15:01 +0100

    2.3: minor refactor in update and on spec

--- src/controllers/update-article.ts
+++ src/controllers/update-article.ts
@@ -5,17 +5,18 @@ import { sendErrorResponse } from '../helpers/utils/send-controller-errors';
 
 const updateArticleById = async (req: Request, res: Response) => {
   try {
-    const { title, text } = req.body;
-    let infoToUpdate = { title };
-    if (text) infoToUpdate = Object.assign(infoToUpdate, { text });
-
     const article = await Article.findById(req.params.id);
     if (!article) {
       const customError = { code: StatusCodes.NOT_FOUND, message: 'Article not found!' };
       return sendErrorResponse(res, customError);
     }
 
-    await Article.updateOne({ _id: req.params.id }, { $set: infoToUpdate }, { new: true });
+    const updatedInfo = {
+      title: req.body.title,
+      text: req.body.text || article.text,
+    };
+
+    await Article.updateOne({ _id: req.params.id }, { $set: updatedInfo }, { new: true });
     return res.status(StatusCodes.NO_CONTENT).json();
   } catch (err) {
     return sendErrorResponse(res, err);
