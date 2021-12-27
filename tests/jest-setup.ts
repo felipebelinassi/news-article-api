7bfe0009abd55903b242b95c39c21ac2fc702afa
Felipe Belinassi <felipebelinassi@gmail.com>
2021-12-27 01:57:19 +0100

    2.2: add db model and prepare jest setup with in-memory db

--- tests/jest-setup.ts
+++ tests/jest-setup.ts
@@ -1,14 +1,23 @@
 import { Server } from 'http';
 import supertest from 'supertest';
+import { MongoMemoryServer } from 'mongodb-memory-server';
 import * as server from '../src/server/app';
+import testConfig from '../config/test';
 
 let jestServer: Server;
+let mongoServer: MongoMemoryServer;
 
 beforeAll(async () => {
-  jestServer = server.start(8081);
+  mongoServer = await MongoMemoryServer.create();
+
+  jestServer = server.start({
+    ...testConfig,
+    database: { url: mongoServer.getUri() },
+  });
   global.testRequest = supertest(jestServer);
 });
 
 afterAll(async () => {
   await server.close(jestServer);
+  await mongoServer.stop();
 });
